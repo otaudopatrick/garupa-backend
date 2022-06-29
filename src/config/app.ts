eab564cec8644ca3c4e92422197c5efde194695f
Patrick Luz <patrickluz.jobs@gmail.com>
2022-06-29 05:54:24 -0300

    chore: add body parser

--- src/config/app.ts
+++ src/config/app.ts
@@ -1,6 +1,8 @@
 import express, { Express } from 'express'
+import setupMiddlewares from '../config/middlewares'
 
 export const setupApp = async ():Promise<Express> => {
   const app = express()
+  setupMiddlewares(app)
   return app
 }
